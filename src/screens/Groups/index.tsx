23d4e8ecc89d22a76f50332fbbfce1248aee210c
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-09-18 09:55:25 -0300

    Estrutura inicial do projeto

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, StatusBar } from "react-native";
+
+export function Group(){
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
