82a5b037159d26b90fb48cd8f4430028d1a6daf3
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-09-23 21:07:05 -0300

    Biblioteca de ícones

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Container, Logo } from './styles';
+import { BackButton, BackIcon, Container, Logo } from './styles';
+
 
 import logoImg from '@assets/logo.png';
 
-export function Header(){
+export function Header() {
   return (
     <Container>
+      <BackButton>
+        <BackIcon />
+      </BackButton>
       <Logo source={logoImg} />
     </Container>
   );
