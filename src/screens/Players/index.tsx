360517c270d7ad34b04642028fc5a7ee4ae5a5fa
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-10-01 19:06:29 -0300

    Criando o componente Icon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,17 +1,29 @@
 import { Header } from '@components/Header';
-import { Container } from './styles';
+import { Container, Form } from './styles';
 import { Highlight } from '@components/Highlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
 
-export function Players(){
-  return(
-  
+
+export function Players() {
+  return (
+
     <Container>
       <Header showBackButton />
-      <Highlight 
+      <Highlight
         title="Nome da Turma"
         subtitle="adicione a galera e separe os times"
       />
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon
+          icon="add"
+        />
+      </Form>
     </Container>
-    
+
   );
 }
