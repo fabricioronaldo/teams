8bfc949a0cf59d3fdba9421151a1e3ff4597f242
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-10-04 16:50:33 -0300

    Implementando Stack Navigator

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,31 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Group } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen
+        name="groups"
+        component={Group}
+      />
+
+      <Screen
+        name="new"
+        component={NewGroup}
+      />
+
+      <Screen
+        name="players"
+        component={Players}
+      />
+
+    </Navigator>
+
+
+  );
+}
